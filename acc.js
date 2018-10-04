0f225977e7b3b158400b96e9c9c231bd3c279f13
Ismail Cucevic <cucevicismail@gmail.com>
2018-10-04 21:40:41 +0000

    add a new web project

---
+++ acc.js
@@ -0,0 +1,56 @@
+var express     =  require("express"),
+    app         =  express(),
+    bodyParser  =  require("body-parser"),
+    mongoose    =  require("mongoose");
+//App config
+mongoose.connect("mongodb://localhost/books_app_demo");
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));   
+
+
+// MONGOOSE/MODEL CONFIG
+var bookSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    author: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Book = mongoose.model("Book",bookSchema);
+
+app.get("/",function(req, res){
+    res.redirect("/books");
+});
+
+
+//Index Routes
+app.get("/books",function(req, res){
+    Book.find({},function(err,books){
+        if(err){
+            console.log("Error!");
+        } else {
+            res.render("index", {books: books});
+        }
+    });
+});
+
+//New Route
+app.get("/books/new", function(req, res){
+    res.render("new");
+});
+//Create Route
+app.post("/books", function(req, res){
+     Book.create(req.body.blog,function(err, newBook){
+         if(err){
+             res.render("new");
+         } else {
+             res.redirect("/books");
+         }
+     });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is running!");
+});
