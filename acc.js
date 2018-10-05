4a6427b2f98ba305b642a6e62a1e1178f0273704
Ismail Cucevic <cucevicismail@gmail.com>
2018-10-05 12:13:15 +0000

    add Delete and Edit buttons

--- acc.js
+++ acc.js
@@ -1,13 +1,16 @@
-var express     =  require("express"),
-    app         =  express(),
-    bodyParser  =  require("body-parser"),
-    mongoose    =  require("mongoose");
+var express         =  require("express"),
+    methodOverride  = require("method-override"),
+    app             =  express(),
+    bodyParser      =  require("body-parser"),
+    expressSanitizer= require("express-sanitizer"),
+    mongoose        =  require("mongoose");
 //App config
 mongoose.connect("mongodb://localhost/books_app_demo");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));   
-
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var bookSchema = new mongoose.Schema({
@@ -42,6 +45,9 @@ app.get("/books/new", function(req, res){
 });
 //Create Route
 app.post("/books", function(req, res){
+    console.log(req.body);
+    console.log("======================");
+    console.log(req.body);
      Book.create(req.body.blog,function(err, newBook){
          if(err){
              res.render("new");
@@ -59,6 +65,40 @@ app.get("/books/:id",function(req, res) {
            res.render("show",{book:showTemp});
        }
     });
+});
+//Edit Route 
+app.get("/books/:id/edit",function(req, res){
+    Book.findById(req.params.id,function(err,foundBook){
+        if(err){
+            req.redirect("/books");
+        }else {
+            res.render("edit",{book:foundBook});
+        }
+    });
+});
+//Update Route
+app.put("/books/:id",function(req, res){
+                                  //(id ,      data,       ,err)
+     req.body.blog.body = req.sanitize(req.body.blog.body)
+    Book.findByIdAndUpdate(req.params.id, req.body.blog,function(err,updatedBook){
+        if(err){
+            req.redirect("/books");
+        }else {
+            res.redirect("/books/" + req.params.id);
+        }
+    });
+});
+
+//Delete Route
+app.delete("/books/:id", function(req,res){
+  Book.findByIdAndRemove(req.params.id,function(err){
+      if(err){
+           res.redirect("/books");
+         }else{
+          res.redirect("/books");
+        }
+  });
+    
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
